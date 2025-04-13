85a953628e6853400cd448a308a749d06e7b86af
Anshu Mittal <anshumittal200421@gmail.com>
2025-04-13 15:27:10 +0530

    response of json data

--- index.js
+++ index.js
@@ -2,6 +2,41 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 4000;
+const githubData = {
+    "login": "Anshumittal123",
+    "id": 122861148,
+    "node_id": "U_kgDOB1K2XA",
+    "avatar_url": "https://avatars.githubusercontent.com/u/122861148?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Anshumittal123",
+    "html_url": "https://github.com/Anshumittal123",
+    "followers_url": "https://api.github.com/users/Anshumittal123/followers",
+    "following_url": "https://api.github.com/users/Anshumittal123/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Anshumittal123/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Anshumittal123/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Anshumittal123/subscriptions",
+    "organizations_url": "https://api.github.com/users/Anshumittal123/orgs",
+    "repos_url": "https://api.github.com/users/Anshumittal123/repos",
+    "events_url": "https://api.github.com/users/Anshumittal123/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Anshumittal123/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": "Anshu Mittal ",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 27,
+    "public_gists": 0,
+    "followers": 4,
+    "following": 32,
+    "created_at": "2023-01-17T07:10:43Z",
+    "updated_at": "2025-04-09T06:22:18Z"
+  }
 
 app.get('/', (req, res)=>{
     res.send('Hello World!')
@@ -17,6 +52,10 @@ app.get('/login', (req, res)=>{
 
 app.get('/youtube', (req, res)=>{
     res.send('<h2>Chai aur code</h2>')
+})
+
+app.get('/github', (req, res)=>{
+    res.json(githubData);
 })
 
 app.listen(process.env.PORT, ()=>{
